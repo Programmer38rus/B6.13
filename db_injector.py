eadd8d279cd4c5cade6641f3b0f7bcc28c2a7aab
Vyacheslav <56330919+Programmer38rus@users.noreply.github.com>
2020-04-18 15:46:41 +0800

    Add files via upload

--- db_injector.py
+++ db_injector.py
@@ -4,9 +4,29 @@ import sqlalchemy as sql
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Integer, String, MetaData, Table
+from bottle import HTTPError
 
 path = "sqlite:///music.sqlite3"
 Base = declarative_base()
+
+
+class InvalidAlbum(HTTPError):
+    pass
+
+
+class BadRequest(InvalidAlbum):
+    """ошибка 400"""
+    pass
+
+
+class Conflict(InvalidAlbum):
+    """ошибка 409"""
+    pass
+
+
+class NotFound(InvalidAlbum):
+    """ошибка 404"""
+    pass
 
 
 def create_db(path):
@@ -26,6 +46,34 @@ def create_db(path):
     metadata.create_all(sql.create_engine(path))
 
 
+def valid_artist(albums_list):
+    """
+    Функция, в случае если список пуст
+    поднимет пользовательскую ошибку NotFound
+    """
+    if not albums_list:
+        raise NotFound()
+    # если совпадения были, мы возвращаем True
+    return True
+
+
+def valid_album(album):
+    """
+    Функция выводит  True для try из модуля "server.py" и перехватывает ошибки 400 и 409 в случае необходимости
+    """
+    if album.year != None:
+        # если year состоит из цифра а длинна строки не привышает 4-х символов
+        if not album.year.isdigit() or len(album.year) > 4 or len(album.year) < 1:
+            raise BadRequest(400, "Введите год цифрами, в формате - 2009")
+
+    # условие выполняется если обект name пришел в виде None из-за отсутствия совпадений по введенному альбому
+    name = db.query(Albums).filter(Albums.album == album.album).first()
+
+    # вызываем ошибку Conflict если name не None
+    if name:
+        raise Conflict(409, "Альбом с таким именем уже существует")
+
+    return True
 
 
 def con_db(path):
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 # Импортируем модуль работы с нашей базой
 import db_injector
+from db_injector import valid_album
 
 from bottle import run
 from bottle import request
 from bottle import route
 from bottle import HTTPError
+
 
 # обрабатываем путь и атрибут
 @route('/albums/<artist>')
@@ -13,17 +15,22 @@ def albums(artist):
     Ищем исполнителя по атрибуту и считаем количество его альбомов.
     после чего выводим количество и список альбомов в браузер
     """
+    # формируем оъект из совпадений - артист из атрибута с артистом из базы
     albums_list = db_injector.find(artist.title())
-    if not albums_list:
-        message = "Альбомов от исполнителя {} не найдено".format(artist.upper())
-        result = HTTPError(404, message)
+    # используем try-except для обнаружения 404 ошибки
+    try:
+        # если артист был найден вернется True и сразу перейдем в блок else
+        db_injector.valid_artist(albums_list)
+    except db_injector.NotFound:
+        # если совпадений не найдено вызываем HTTPError
+        result = HTTPError(404, "Альбомов от исполнителя {} не найдено".format(artist.upper()))
     else:
-
         albums_names = [album.album for album in albums_list]
         result = "Количество найденныйх альбомов {}: {} <br><br>".format(artist.upper(), len(albums_list))
         result += "<br>".join(albums_names)
 
     return result
+
 
 # обрабатываем POST запрос
 @route('/albums', method="POST")
@@ -38,25 +45,25 @@ def add_album():
         album=request.forms.get("album").title()
     )
 
-    # создаём объект с первым попавшимся совпадением по альбомам! Если совпадений нет
-    # объект возращается ввиде None
-    name = db_injector.db.query(db_injector.Albums).filter(db_injector.Albums.album == album.album).first()
+    # Запускаем валидацию введенных данные в album
+    # и перехватываем пользовательские ошибки в случае их возникновения
+    try:
+        valid_album(album)
 
-    # если year состоит из цифра а длинна строки не привышает 4-х символов
-    if not album.year.isdigit() or len(album.year) > 4:
-        result = HTTPError(415, "Введите год цифрами, в формате - 2009")
+    except db_injector.BadRequest as err:
 
-    # условие выполняется если обект name пришел в виде None
-    elif not name:
+        result = HTTPError(400, "Введите год цифрами, в формате - 2009")
+
+    except db_injector.Conflict as err:
+
+        result = HTTPError(409, "Альбом с таким именем уже существует")
+
+    else:
         db_injector.commit(album)
         result = "Был добавлен новый альбом \nГод - {}\nИсполнитель - {} \nЖанр - {} \nАльбом - {}".format(album.year,
                                                                                                            album.artist,
                                                                                                            album.genre,
                                                                                                            album.album)
-    else:
-        message = "Альбом с таким именем уже существует"
-        result = HTTPError(409, message)
-
     return result
 
 
